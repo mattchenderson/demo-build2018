3bec4de7eb0ee3bceffedfc136fe6f01065288f0
Matthew Henderson <mahender@microsoft.com>
2018-05-06 11:51:48 -0700

    setting up linux web app

---
+++ thr2004/LinuxWebApp/LinuxAuthDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinuxAuthDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
