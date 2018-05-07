bd455e3da05a97d3051bd6a84c1401fd09099052
Matthew Henderson <mahender@microsoft.com>
2018-05-06 17:09:28 -0700

    storage explorer

--- setup.ps1
+++ setup.ps1
@@ -3,3 +3,4 @@
 
 start microsoft-edge:https://portal.azure.com?feature.customportal=false
 explorer.exe .\pictures
+start ${Env:ProgramFiles(x86)}\"Microsoft Azure Storage Explorer"\StorageExplorer.exe
